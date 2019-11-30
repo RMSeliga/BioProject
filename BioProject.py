dfe51c7989c38656fb1cf6a5ee72221953b3260f
Robert Seliga <robertmseliga@gmail.com>
2019-11-30 16:59:40 -0500

    init commit

---
+++ BioProject.py
@@ -0,0 +1,13 @@
+#Reader Function: Reads the sequence and the associated probabilities.
+#Could work one of two ways
+#1: Creates two arrays of the same length as the sequence, each index has the corresponding nucleotide and probability
+#2: Creates one array of tuples with length equal to the sequence, each tuple contains the nucleotide + probability (could probably also do with sets for better runtime)
+
+#Finder/Matcher
+#This should use the blast algorithm with a small addition
+#Basically it does the standard best fit search to find the most likely origin point for the query sequence, only now the confidence value also incorporates the probabilites
+#I.E. at each point, when comparing nucleotides from the query to the dictionary if the probability of the dictionary nucleotide being correct is low, then the confidence value from that index should also be low
+#E.G a matching that has lots of matches but very low probabilities for those matches will be less likely to be selected than a matching with fewer matches, but very high probabilites at those matches
+#This should also be true for the opposite case where there is a mismatch
+
+ 
