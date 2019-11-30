7ca721e9d999b75759692a63c0a3648eac15f2b8
Robert Seliga <robertmseliga@gmail.com>
2019-11-30 18:48:19 -0500

    Created the reader function

--- BioProject.py
+++ BioProject.py
@@ -2,7 +2,22 @@
 #Could work one of two ways
 #1: Creates two arrays of the same length as the sequence, each index has the corresponding nucleotide and probability
 #2: Creates one array of tuples with length equal to the sequence, each tuple contains the nucleotide + probability (could probably also do with sets for better runtime)
+sequence = []
+def Reader(file1, file2):
+	f = open(file1)
+	for line in f:
+		for nucleotide in line:
+			sequence.append([nucleotide,None])
+	print len(sequence)
+	
+	i=0;
+	f2 = open(file2)
+	for line in f2:
+		for p in line.split():
+			sequence[i][1]=p
+			i+=1
 
+Reader("sequence.txt", "probabilities.txt")
 #Finder/Matcher
 #This should use the blast algorithm with a small addition
 #Basically it does the standard best fit search to find the most likely origin point for the query sequence, only now the confidence value also incorporates the probabilites
